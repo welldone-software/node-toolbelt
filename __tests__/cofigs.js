1e3f29d8b786f9941068c04c96ea7eff70be9d86
EladHeller <eladheller@gmail.com>
2017-12-21 22:24:44 +0200

    reuse tests

--- __tests__/cofigs.js
+++ __tests__/cofigs.js
@@ -1,23 +1,27 @@
 const {mapEnv, assignEnv} = require('../src/lib/configs')
 
+const newObjectTest = () => {
+  const a = {}
+  expect(mapEnv(a)).not.toBe(a)
+  expect(mapEnv(a) !== a).toBeTruthy()
+}
+
+const replaceKeysTest =  () => {
+  const a = {}
+  expect(assignEnv(a)).toBe(a)
+  expect(assignEnv(a) === a).toBeTruthy()
+}
+
 describe('mapEnv', () => {
-  test('returns a new object', () => {
-    const a = {}
-    expect(mapEnv(a)).not.toBe(a)
-    expect(mapEnv(a) !== a).toBeTruthy()
-  })
-  test('replace keys with env keys', ()=>{
-    process.env.TEST = 'hello env'
-    const res = mapEnv({TEST:'hello world'})
-    expect(res.TEST === 'hello env').toBeTruthy()
-  })
+
+  test('returns a new object', newObjectTest)
+  test('replace keys with env keys', replaceKeysTest)
+  
 })
 
 describe('assignEnv', () => {
-  test('returns the same object', () => {
-      const a = {}
-      expect(assignEnv(a)).toBe(a)
-      expect(assignEnv(a) === a).toBeTruthy()
-  })
+
+  test('returns the same object', newObjectTest)
+  test('replace keys with env keys', replaceKeysTest)
 
 })
