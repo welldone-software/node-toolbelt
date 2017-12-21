7875ba5108d1d323c9a8f6474ea89c7bc3853039
Elad Heller <eladheller@gmail.com>
2017-12-21 21:04:28 +0200

    replace keys test

--- __tests__/cofigs.js
+++ __tests__/cofigs.js
@@ -1,17 +1,19 @@
 const {mapEnv, assignEnv} = require('../src/lib/configs')
 
 describe('mapEnv', () => {
-
   test('returns a new object', () => {
     const a = {}
     expect(mapEnv(a)).not.toBe(a)
     expect(mapEnv(a) !== a).toBeTruthy()
   })
-
+  test('replace keys with env keys', ()=>{
+    process.env.TEST = 'hello env'
+    const res = mapEnv({TEST:'hello world'})
+    expect(res.TEST === 'hello env').toBeTruthy()
+  })
 })
 
 describe('assignEnv', () => {
-
   test('returns the same object', () => {
       const a = {}
       expect(assignEnv(a)).toBe(a)
