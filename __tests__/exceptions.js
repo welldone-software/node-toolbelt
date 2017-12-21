e08bc9972cc43a0583fd84a3a9a7db8ab800a25b
EladHeller <eladheller@gmail.com>
2017-12-21 23:11:50 +0200

    use ctor message

--- __tests__/exceptions.js
+++ __tests__/exceptions.js
@@ -18,30 +18,47 @@ const initDefaultValues=(error, httpErrorType)=>{
         context: undefined })
 }
 
+const useMessage = (error, httpErrorType)=>{
+    const msg = 'some text'
+    const err = new error(msg)
+    expect(err).toEqual({
+        message: msg,
+        name: error.name,
+        code: HttpError[_.snakeCase(httpErrorType).toUpperCase()],
+        context: undefined })
+}
+
 describe('AlreadyExistsError',()=>{
     test('has correct default values',()=>initDefaultValues(AlreadyExistsError, 'Conflict'))
+    test('use message',()=>useMessage(AlreadyExistsError, 'Conflict'))
 })
 
 describe('AuthorizationError',()=>{
     test('has correct default values',()=>initDefaultValues(AuthorizationError, 'Unauthorized'))
+    test('use message',()=>useMessage(AlreadyExistsError, 'Conflict'))
 })
 
 describe('AuthenticationError',()=>{
     test('has correct default values',()=>initDefaultValues(AuthenticationError, 'Unauthorized'))
+    test('use message',()=>useMessage(AlreadyExistsError, 'Conflict'))
 })
 
 describe('InvalidArgumentError',()=>{
     test('has correct default values',()=>initDefaultValues(InvalidArgumentError, 'Bad Request'))
+    test('use message',()=>useMessage(AlreadyExistsError, 'Conflict'))
 })
 
 describe('InvalidStateError',()=>{
     test('has correct default values',()=>initDefaultValues(InvalidStateError, 'Bad Request'))
+    test('use message',()=>useMessage(AlreadyExistsError, 'Conflict'))
 })
 
 describe('NotFoundError',()=>{
     test('has correct default values',()=>initDefaultValues(NotFoundError, 'Not Found'))
+    test('use message',()=>useMessage(AlreadyExistsError, 'Conflict'))
 })
 
 describe('UnexpectedError',()=>{
     test('has correct default values',()=>initDefaultValues(UnexpectedError, 'Internal Server Error'))
+    test('use message',()=>useMessage(AlreadyExistsError, 'Conflict'))
 })
