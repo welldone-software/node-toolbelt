0ba28c312baab57292d4e571d328e16f5fe67330
Giora Guttsait <giora111@gmail.com>
2017-12-24 19:36:03 +0200

    Add body to request logger

--- src/lib/loggers.js
+++ src/lib/loggers.js
@@ -2,6 +2,11 @@ const pino = require('pino')
 const {assignWith} = require('lodash')
 const expressPino = require('express-pino-logger')
 
+const requestSerializer = req => ({
+  ...pino.stdSerializers.req(req),
+  body: req.body,
+})
+
 const assignWithCustomizer = (objValue, srcValue) =>
     (objValue === undefined ? srcValue : objValue)
 
@@ -18,18 +23,13 @@ const errSerializer = err =>
 
 const logger = pino({
   serializers: {
+    req: requestSerializer,
     err: errorSerializer,
     error: errorSerializer,
   },
 })
 
-const expressLogger = expressPino({
-  logger,
-  // serializers: {
-  //   err: errorSerializer,
-  //   error: errorSerializer,
-  // },
-})
+const expressLogger = expressPino({logger})
 
 module.exports = {
   logger,
