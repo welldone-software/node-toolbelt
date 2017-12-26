484ae53a1ba24f2447e512f2d17d2c0984de3df6
Elad Heller <eladheller@gmail.com>
2017-12-26 13:08:10 +0200

    logger fixes and tests

--- src/lib/loggers.js
+++ src/lib/loggers.js
@@ -1,48 +1,50 @@
 const pino = require('pino')
-const {assignWith, forEach, cloneDeep} = require('lodash')
+const {assignWith, mapValues, map} = require('lodash')
 const expressPino = require('express-pino-logger')
-const { IncomingMessage, ServerResponse } = require('http')
+const {IncomingMessage} = require('http')
 
 const assignWithCustomizer = (objValue, srcValue) =>
-    (objValue === undefined ? srcValue : objValue)
+  (objValue === undefined ? srcValue : objValue)
 
-const errSerializer = err => 
-  err instanceof Error ?
-    assignWith(
-        {
-          type: err.constructor.name,
-          message: err.message,
-          stack: err.stack,
-        },
-        err,
-        assignWithCustomizer
-    ) : err
+const errSerializer = err =>
+  (err instanceof Error
+    ? assignWith(
+      {
+        type: err.constructor.name,
+        message: err.message,
+        stack: err.stack,
+      },
+      err,
+      assignWithCustomizer
+    )
+    : err)
 
-const removeDeep = (obj, key) => {
-  delete obj[key]
-  forEach(obj, (v) => {
-      return typeof v == "object" ? removeDeep(v, key) : []
+const removeDeep = (obj, keys) => {
+  const fn = obj instanceof Array ? map : mapValues
+  return fn(obj, (v, k) => {
+    if (keys.indexOf(k) === -1) {
+      return typeof v === 'object' ? removeDeep(v, keys) : v
+    }
   })
 }
 
-const parseBody = (req, options) =>{
+const parseBody = (req, options) => {
   let body
-  const useBody = req.headers['content-type'].split(';').some(type => {
-    switch (type.trim()){
-      case 'application/json': case 'application/x-www-form-urlencoded':
-        return true
-      default:
-        return false
-    }
-  })
-  if (useBody) {
+
+  const contetType = req.headers['content-type']
+  const useBody =
+    contetType &&
+    /application\/json|application\/x-www-form-urlencoded/i.test(contetType)
+
+  if (useBody && req.raw.body) {
     body = req.raw.body
-    
-    if (options.removeSensitiveFields)
-      options.sensitiveFields.forEach(field=> removeDeep(req.raw.body, field))
+
+    if (options.removeSensitiveFields) {
+      body = removeDeep(body, options.sensitiveFields)
+    }
 
     if (req.headers['content-length'] > options.maxBodySize) {
-      if (typeof body !== 'string'){
+      if (typeof body !== 'string') {
         body = JSON.stringify(body)
       }
       body = `${body.slice(0, options.maxBodySize - 3)}...`
@@ -51,39 +53,47 @@ const parseBody = (req, options) =>{
   return body
 }
 
-const requestSerializer = (options= {}, req) => {
-  const reqClone = cloneDeep(req)
+const parseHeaders = (headers, opts) =>
+  (opts.removeSensitiveFields
+    ? removeDeep(headers, opts.sensitiveHeaders)
+    : headers)
+
+const requestSerializer = (options = {}, req) => {
   const defaultOptions = {
-    maxBodySize: 1024, 
-    removeSensitiveFields: true, 
-    sensitiveHeaders: ['authorization','proxy-authorization '], 
-    sensitiveFields: ['password']
+    maxBodySize: 1024,
+    removeSensitiveFields: true,
+    sensitiveHeaders: ['authorization', 'proxy-authorization'],
+    sensitiveFields: ['password'],
   }
   const opts = {...defaultOptions, ...options}
 
-  if (opts.removeSensitiveFields)
-    opts.sensitiveHeaders.forEach(header=> delete reqClone.headers[header])
-
-  return (reqClone && reqClone.raw instanceof IncomingMessage) ? 
-    assignWith(
+  return req && req.raw instanceof IncomingMessage
+    ? assignWith(
       {
-        body: parseBody(reqClone, opts),
+        body: parseBody(req, opts),
+        headers: parseHeaders(req.headers, opts),
       },
-      reqClone,
+      req,
       assignWithCustomizer
-    ) : req
+    )
+    : req
 }
 
-const serializers = (options)=> ({
+const serializers = options => ({
   req: requestSerializer.bind(null, options),
   err: errSerializer,
   error: errSerializer,
 })
 
-const logger = pino({serializers:serializers()})
-const expressLogger = (options)=> expressPino({logger, serializers:serializers(options)})
+const logger = pino({serializers: serializers()})
+const expressLogger = options =>
+  expressPino({logger, serializers: serializers(options)})
 
 module.exports = {
   logger,
   expressLogger,
+  _errSerializer: errSerializer,
+  _parseBody: parseBody,
+  _requestSerializer: requestSerializer,
+  _removeDeep: removeDeep,
 }
